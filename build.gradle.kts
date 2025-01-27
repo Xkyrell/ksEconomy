8acc9cb012db33da7a3eaa9a62fd3db2d7364887
xkyrell <xkyrell@gmail.com>
2025-01-27 23:27:36 +0200

    Improve gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,18 +19,15 @@ repositories {
 dependencies {
     compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
-    implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("com.google.code.gson:gson:2.8.8")
-
-    compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
-    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+    compileOnly("com.zaxxer:HikariCP:5.0.1")
+    compileOnly("com.google.code.gson:gson:2.8.8")
     compileOnly("me.clip:placeholderapi:2.11.6")
-
-    implementation("com.google.dagger:dagger:2.48")
+    compileOnly("com.google.dagger:dagger:2.48")
     annotationProcessor("com.google.dagger:dagger-compiler:2.48")
 
-    testImplementation("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
-    testImplementation("com.github.MilkBowl:VaultAPI:1.7")
+    implementation("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+    implementation("com.github.MilkBowl:VaultAPI:1.7")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.mockito:mockito-core:4.8.0")
@@ -51,6 +48,16 @@ tasks {
     withType<ShadowJar> {
         archiveFileName.set("${rootProject.name}-${rootProject.version}.jar")
         destinationDirectory.set(rootProject.rootDir.resolve("build"))
+
+        exclude("net/**")
+        exclude("com/**")
+        exclude("org/**")
+        exclude("co/**")
+        exclude("io/**")
+        exclude("junit/**")
+        exclude("javax/**")
+
+        exclude("**/META-INF/**", "**/mojang-translations/**", "**/LICENSE.txt")
     }
 
     withType<JavaCompile> {
@@ -65,4 +72,9 @@ bukkit {
     main = "me.xkyrell.kseconomy.EconomyPlugin"
     softDepend = listOf("PlaceholderAPI", "Vault")
     apiVersion = "1.16"
+    libraries = listOf(
+        "com.zaxxer:HikariCP:5.0.1",
+        "com.google.dagger:dagger:2.48",
+        "com.google.code.gson:gson:2.8.8"
+    )
 }
